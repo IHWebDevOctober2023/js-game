9b7fcda36cd28c78663ed965dc0cce1b1d34c305
marshall-bits <kz_@hotmail.es>
2023-10-20 13:31:56 +0200

    add collissions and game over

--- index.js
+++ index.js
@@ -1,66 +1,107 @@
 console.log("it's working!");
 
 const ball = document.querySelector("#ball");
+const racket = document.querySelector("#racket");
 // This is going to check the actual width IN THE BROWSER. According to the dimensions of the screen
 // remember the width of the container has a unit in vw so it's adapting it's size depending on the width
 const gameBoardWidth = document.querySelector("#game-board").clientWidth;
 const gameBoardHeight = document.querySelector("#game-board").clientHeight;
-let positionY = 0;
-let positionX = 0;
+const gameBoardElement = document.querySelector("#game-board");
 
-let directionY = "down"
-let directionX = "right"
+let positionYRacket = 0;
+
+let positionYBall = 0;
+let positionXBall = 0;
+
+let directionYBall = "down"
+let directionXBall = "right"
+
+let ballSpeed = 4;
+let racketSpeed = 10;
+
+let animationId;
 
 /* setInterval(()=>{
     position += 30
     ball.style.left = `${position}px`
     ball.style.top = `${position}px`
-},1000) */ 
+},1000) */
 console.log(gameBoardWidth);
-function moveBall(){
+function moveBall() {
 
-    if(directionX === "right"){
-        positionX += 1;
-        if(positionX >= gameBoardWidth - ball.clientWidth){
-            directionX = "left";
+    if (directionXBall === "right") {
+        positionXBall += ballSpeed;
+        if (positionXBall >= gameBoardWidth - ball.clientWidth) {
+            directionXBall = "left";
         }
-    } else if (directionX === "left"){
-        positionX -= 1;
-        if(positionX <= 0){
-            console.log("GAME OVER");
-        }
-    }
-    
-    if(directionY === "down"){
-        positionY += 1;
-        if(positionY >= gameBoardHeight - ball.clientHeight){
-            directionY = "up";
-        }
-        
-    } else if (directionY === "up"){
-        positionY -= 1;
-        if(positionY <= 0){
-            directionY = "down";
+    } else if (directionXBall === "left") {
+        positionXBall -= ballSpeed;
+        if (positionXBall <= 0) {
+            ball.remove()
+            racket.remove()
+
+            const h1Element = document.createElement("h1");
+            h1Element.innerText = "GAME OVER";
+            gameBoardElement.appendChild(h1Element)
+            cancelAnimationFrame(animationId);
         }
     }
 
+    if (directionYBall === "down") {
+        positionYBall += ballSpeed;
+        if (positionYBall >= gameBoardHeight - ball.clientHeight) {
+            directionYBall = "up";
+        }
 
-    ball.style.left = `${positionX}px`
-    ball.style.top = `${positionY}px`
-    
-  
+    } else if (directionYBall === "up") {
+        positionYBall -= ballSpeed;
+        if (positionYBall <= 0) {
+            directionYBall = "down";
+        }
+    }
+
+
+    ball.style.left = `${positionXBall}px`
+    ball.style.top = `${positionYBall}px`
+
+
 }
 
+function moveRacket(event) {
+    console.log("THIS FUNCTION IS CALLED");
+
+    if (event.key === "ArrowUp") {
+        positionYRacket -= 10
+    } else if (event.key === "ArrowDown") {
+        positionYRacket += 10
+    }
+
+    racket.style.top = `${positionYRacket}px`
+}
+
+document.addEventListener("keydown", moveRacket)
+
+function checkForCollissions() {
+    if (0 < positionXBall + ball.clientWidth &&
+        0 + racket.clientWidth > positionXBall &&
+        positionYRacket < positionYBall + ball.clientHeight &&
+        positionYRacket + racket.clientHeight > positionYBall) {
+        directionXBall = "right"
+    }
+}
 
 let frames = 0;
-function animate(){
+function animate() {
     moveBall();
-    frames ++;
+    checkForCollissions();
+    frames++;
 
-/*     if(frames %60 ===0){
-        console.log("Here we increase the difficulty");
-    } */
-    requestAnimationFrame(animate)
+    /*     if(frames %60 ===0){
+            console.log("Here we increase the difficulty");
+        } */
+
+    // We need to store the animation in a variable so we can stop it later
+    animationId = requestAnimationFrame(animate);
 }
 
 animate()
